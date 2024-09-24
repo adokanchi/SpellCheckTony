55f1b2f91de4a4abfe04301a92e1262015ac4dae
adokanchi <tony.dokanchi@menloschool.org>
2024-09-24 15:18:21 -0700

    Att1 p1

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,10 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date         |    Time    |                                                                                                                                                                                                                                                            Update |
+|:-------------|:----------:|------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| April 5      |   9-10pm   |                                                                                                                                                                                                                        I tried to write abstract, efficient code. |
+| September 24 | 11-11:30am |        I built a linear search algorithm, then I tried building a binary search algorithm that wasn't working by the end of the half hour. I don't have a good idea on how to solve this. Maybe some kind of map for lookups? Maybe a tree storing all the words? |
+| September 24 |  2-2:30pm  | I spoke with kids in class to float around ideas. Our best ideas at the moment are hash tables, some sort of map, or a tree that stores objects that hold information about valid words. I'll probably start by trying the tree approach? Not sure at the moment. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -1,10 +1,12 @@
+import java.util.ArrayList;
+
 /**
  * Spell Check
  * A puzzle written by Zach Blick
  * for Adventures in Algorithms
  * At Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Tony Dokanchi
  * */
 
 public class SpellCheck {
@@ -17,8 +19,60 @@ public class SpellCheck {
      * @param dictionary The list of all accepted words.
      * @return String[] of all mispelled words in the order they appear in text. No duplicates.
      */
+
     public String[] checkWords(String[] text, String[] dictionary) {
 
+        int maxLength = 0;
+        String longest = "";
+        for (String word : dictionary) {
+            if (word.length() > maxLength) {
+                maxLength = word.length();
+                longest = word;
+            }
+        }
+        System.out.println(maxLength);
+        System.out.println(longest);
         return null;
+        /*
+        ArrayList<String> wrongWords = new ArrayList<String>();
+        for (String word : text) {
+            if (!inDict(word, dictionary)) {
+                wrongWords.add(word);
+            }
+        }
+
+        String[] wrongWordsArr = new String[wrongWords.size()];
+        int i = 0;
+        while (!wrongWords.isEmpty()) {
+            wrongWordsArr[i] = wrongWords.remove(0);
+            i++;
+        }
+        return wrongWordsArr;
+        */
+    }
+
+    public boolean inDict(String word, String[] dictionary) {
+        int startIndex = 0;
+        int endIndex = dictionary.length;
+
+        // Make dictionary lowercase
+        for (int i = 0; i < word.length(); i++) {
+
+            while (!word.substring(0,i).equals(dictionary[startIndex].substring(0,i))) {
+
+            }
+        }
+        while (!word.equals(dictionary[startIndex])) {
+            int middleIndex = (startIndex + endIndex) / 2;
+            if (dictionary[middleIndex].charAt(0) > word.charAt(0)) {
+                endIndex = middleIndex;
+            }
+            else {
+                startIndex = middleIndex;
+            }
+        }
+
+
+        return false;
     }
 }
